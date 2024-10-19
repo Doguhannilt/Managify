628bf13b2200ae9b17449368de1844e37508854a
doguhannilt <doguhannilt@gmail.com>
2024-10-20 01:32:29 +0300

    JwtTokenValidator added

--- managify-backend/src/main/java/com/econ/managify/config/JwtConstant.java
+++ managify-backend/src/main/java/com/econ/managify/config/JwtConstant.java
@@ -1,4 +1,6 @@
 package com.econ.managify.config;
 
 public class JwtConstant {
+    public static final String SECRET_KEY = "onlylordknowswhatiamdoing";
+    public static final String JWT_HEADER = "Authorization";
 }
