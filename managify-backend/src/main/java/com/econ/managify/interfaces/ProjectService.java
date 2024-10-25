8bf7ab97ef680bb82068e34a75d0007f685bcc68
doguhannilt <doguhannilt@gmail.com>
2024-10-25 03:14:59 +0300

    Updated Chat, Project, and User service interfaces and implementations, and modified repositories

--- managify-backend/src/main/java/com/econ/managify/interfaces/ProjectService.java
+++ managify-backend/src/main/java/com/econ/managify/interfaces/ProjectService.java
@@ -23,5 +23,7 @@ public interface ProjectService {
 
     Chat getChatByProjectId(Long projectId) throws Exception;
 
+    List<Project> searchProjects(String keyword, User user) throws  Exception;
+
 
 }
