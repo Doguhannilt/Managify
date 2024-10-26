800f657ab92a615ce6f910cf563d02bca9cbbe4f
doguhannilt <doguhannilt@gmail.com>
2024-10-26 15:07:10 +0300

    repositories

--- managify-backend/src/main/java/com/econ/managify/repository/IssueRepository.java
+++ managify-backend/src/main/java/com/econ/managify/repository/IssueRepository.java
@@ -1,4 +1,10 @@
 package com.econ.managify.repository;
 
-public class IssueRepository {
+import com.econ.managify.model.Issues;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IssueRepository extends JpaRepository<Issues, Long> {
+    public List<Issues> findByProjectId(Long id);
 }
--- managify-backend/src/main/java/com/econ/managify/repository/SubscriptionRepository.java
+++ managify-backend/src/main/java/com/econ/managify/repository/SubscriptionRepository.java
@@ -1,4 +1,6 @@
 package com.econ.managify.repository;
 
-public interface SubscriptionRepository {
-}
+import com.econ.managify.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> { Subscription findByUserId(Long userId);}
